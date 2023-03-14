ee06d74f922147ff0ecdc7abcab719ff732ba717
Italo Lima <87744963+ItaloLis@users.noreply.github.com>
2023-03-14 11:12:31 -0300

    Update encontrar_pontos.py

--- encontrar_pontos.py
+++ encontrar_pontos.py
@@ -1,4 +1,4 @@
-# Dados 2 pontos, calcular y-ax-b
+# Dados 2 pontos, calcular y=ax-b
 # e decidir onde está um ponto P (=0, <0, > 0)
 
 ponto1 = [3, 4]
@@ -8,7 +8,7 @@ pontoP = [1, 2]
 def sinal(x1, y1, x2, y2, x, y):
     a = (y2 - y1)/(x2 - x1)
     b = y1 - a*x1
-    return y - a*x - b
+    return y - a*x - b #mudar aq
 
 x1 = 3
 y1 = 4
@@ -18,3 +18,32 @@ xp = 1
 yp = 2
 
 print(round(sinal(x1, y1, x2, y2, xp, yp), 2))
+
+
+# Verificar se um ponto P está dentro de um triângulo ABC
+# Triângulo : x1, y1, x2, y2, x3, y3
+# ponto P : x, y
+
+def cadeop(x1, y1, x2, y2, x3, y3, x, y):
+    local1 = sinal(x1, y1, x2, y2, x, y)
+    local2 = sinal(x1, y1, x3, y3, x, y)
+    local3 = sinal(x2, y2, x3, y3, x, y)
+
+    if local1 * local2 * local3 == 0:
+        return 'Dentro'
+    elif [local1, local2, local3].count(-1) == 2:
+        return 'Dentro'
+    else:
+        return 'Fora'
+
+
+x1 = 1
+y1 = 1
+x2 = 0
+y2 = 1
+x3 = 3
+y3 = 0
+x = 0.5
+y = 1.3
+
+print(cadeop(x1, y1, x2, y2, x3, y3, x, y))
